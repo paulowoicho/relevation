d029813062ec22d5fbb3778b7a5e1bb44be72e13
Paul Owoicho <paul.owoicho@aun.edu.ng>
2022-10-14 00:56:26 +0100

    update utlity script

--- utilities/map_run.py
+++ utilities/map_run.py
@@ -1,19 +1,21 @@
 import argparse
-import Path
+from pathlib import Path
 import json
 
 parser = argparse.ArgumentParser(description='Re map a run file')
 parser.add_argument('run_file', type=str)
+parser.add_argument('query_mapping', type=str)
 
 args = parser.parse_args()
 
 run_file_stem = Path(args.run_file).stem
+parent_folder = Path(args.run_file).parent.resolve()
 
-with open('mapping.json') as query_mapping_file:
+with open(args.query_mapping) as query_mapping_file:
     query_mapping = json.load(query_mapping_file)
 
 
-with open(f"{run_file_stem}_mapped.run", "w") as updated_run:
+with open(f"{parent_folder}/{run_file_stem}_mapped.run", "w") as updated_run:
     with open(args.run_file) as original_run:
         for line in original_run:
             qid, Q0, docid, rank, score, run_name = line.split("\t")
